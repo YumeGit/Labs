019a67f8b941df79efbccc5dc97da4067604f098
shoxcie <resetnicov96@gmail.com>
2023-11-06 20:48:32 +0200

    Lab5

---
+++ Lab5/Modul.py
@@ -0,0 +1,48 @@
+import math
+
+def calculate_circle_area(radius):
+    """
+    Рассчитывает площадь круга по заданному радиусу.
+    
+    Аргументы:
+    radius (float): Радиус круга.
+    
+    Возвращает:
+    float: Площадь круга.
+    """
+    return math.pi * radius**2
+
+def calculate_circle_diameter(radius):
+    """
+    Рассчитывает диаметр круга по заданному радиусу.
+    
+    Аргументы:
+    radius (float): Радиус круга.
+    
+    Возвращает:
+    float: Диаметр круга.
+    """
+    return 2 * radius
+
+def calculate_circle_radius(diameter):
+    """
+    Рассчитывает радиус круга по заданному диаметру.
+    
+    Аргументы:
+    diameter (float): Диаметр круга.
+    
+    Возвращает:
+    float: Радиус круга.
+    """
+    return diameter / 2
+
+# Пример использования функций
+radius = 5.0
+diameter = calculate_circle_diameter(radius)
+area = calculate_circle_area(radius)
+calculated_radius = calculate_circle_radius(diameter)
+
+print(f"Радиус: {radius}")
+print(f"Диаметр: {diameter}")
+print(f"Площадь круга: {area}")
+print(f"Рассчитанный радиус (по диаметру): {calculated_radius}")
